026571b0642813a4a30cc9325677e096628a0661
Gift <aghaulor.gift@gmail.com>
2024-08-31 18:46:16 +0000

    filter function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,9 @@
 """A  function called filter_datum that returns the log message obfuscated"""
 import re
 
-def filter_datum(fields, redaction, message, separator):
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """filter_datum that returns the log message obfuscated"""
     pattern = f"({'|'.join(fields)})=([^\\{separator}]+)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
