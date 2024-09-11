c6a5e3d199ea4834c9b3c093bf54a6e254ca5fbf
Gift <aghaulor.gift@gmail.com>
2024-09-11 20:29:29 +0000

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,6 @@
 """A simple Flask app with user authentication features.
 """
 from flask import Flask, jsonify, request, abort, redirect
-
 from auth import Auth
 
 
@@ -14,6 +13,7 @@ AUTH = Auth()
 def index() -> str:
     """Returns a JSON response with a welcome message."""
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
