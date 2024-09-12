43cb14cc9a402fd0ef57a36bfff6a839eb25ea00
Gift <aghaulor.gift@gmail.com>
2024-09-12 11:05:03 +0000

    Create session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,3 +66,15 @@ class Auth:
         except NoResultFound:
             return False
         return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session for a user identified by email,
+        returning the session ID."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
