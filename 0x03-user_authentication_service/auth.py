37546aa8ff7ce3ed63f4f935d9e439836fff539a
Gift <aghaulor.gift@gmail.com>
2024-09-12 10:35:03 +0000

    Add a private uuid method in auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 and returns bytes.The returned bytes is a salted hash of the input password,
 hashed with bcrypt.hashpw."""
 import bcrypt
+import uuid
 from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
@@ -23,16 +24,16 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+def _generate_uuid() -> str:
+    """Generate a new UUID and return its string representation."""
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database."""
 
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """Hashes a password using bcrypt."""
-        salt = bcrypt.gensalt()
-        return bcrypt.hashpw(password.encode('utf-8'), salt)
 
     def register_user(self, email: str, password: str) -> User:
         """Registers a new user with the given email and password.
